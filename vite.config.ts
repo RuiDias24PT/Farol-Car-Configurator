b8c89d8a489fbe091fca569d99197ee70ba9c2fb
Rui <rmsddx24@gmail.com>
2026-09-01 11:27:00 +0100

    chore: add '@/' path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import { fileURLToPath } from 'node:url'
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
